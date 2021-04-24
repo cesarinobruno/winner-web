17eb3981d1231fea0c4d61296c9de376735461e3
cesarinobruno <bruno.rjsantos@hotmail.com>
2021-04-24 19:58:04 -0300

    Adicionando nova rota

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import React from "react";
 import Home from "../pages/Home";
 import Login from "../pages/Login/Login";
+import Novo from '../pages/NewUser/admin';
 
 function MainRoutes(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/novoUsuario" element={<Novo />}/>
     </Routes>
   );
 }
