c8d0881f3e305454c775ed7c8b87fef93f63e847
brunosmauro <brunomtb08@gmail.com>
2021-05-30 09:25:03 -0300

    Inserção da avaliação na home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import "./style.css";
-import { Button, Modal } from "react-bootstrap";
+import { Button, Modal, Nav, NavDropdown } from "react-bootstrap";
 import Logo from "../../assets/logo.png";
 import Bitmap from "../../assets/bitmap.png";
 import ButtonMap from "../../components/ButtonMap";
 import DistrictUpa from "../../components/District";
+import { title } from "node:process";
 
 
 const Home: React.FC = () => {
@@ -44,15 +45,52 @@ const Home: React.FC = () => {
   // Plotly.newPlot('grafico', data);
   const districtUpas = [
     {
-      title: "centro",
+      title: "Centro",
       atendimentos: 2000,
+    },
+    {
+      title: "Rocinha",
+      atendimentos: 1506,
+    },
+    {
+      title: "Lins",
+      atendimentos: 1200,
+    },
+    {
+      title: "Penha",
+      atendimentos: 1100,
+    },
+    {
+      title: "Tijuca",
+      atendimentos: 1700,
+    },
+    {
+      title: "Madureira",
+      atendimentos: 1520,
+    },
+    {
+      title: "Barra da Tijuca",
+      atendimentos: 1322,
+    },
+    {
+      title: "Bangu",
+      atendimentos: 1750,
+    },
+    {
+      title: "Campo Grande",
+      atendimentos: 1660,
+    },
+    {
+      title: "Santa Cruz",
+      atendimentos: 1740,
     },
   ];
   return (
     <div className="container-root">
-        {/* <div className="page-header"> */}
-        <ul className="nav nav-tabs ml-1" role="tablist">
-          <li role="presentation" className="active">
+      {/* <div className="page-header"> */}
+
+      <ul className="nav nav-tabs ml-1" role="tablist">
+        {/* <li role="presentation" className="active">
             <a
               href="#disponiveis"
               aria-controls="disponiveis"
@@ -61,166 +99,206 @@ const Home: React.FC = () => {
             >
               <span className="tabsNavigation ml-1">UPAs disponíveis</span>
             </a>
-          </li>
-          <li role="presentation">
-            <a
-              href="#graficoatend"
-              aria-controls="graficoatend"
-              role="tab"
-              data-toggle="tab"
-            >
-              <span className="tabsNavigation">Gráfico Atendimentos</span>
-            </a>
-          </li>
-          <li role="presentation">
-            <a
-              href="#endereco"
-              aria-controls="endereco"
-              role="tab"
-              data-toggle="tab"
-            >
-              <span className="tabsNavigation">Endereços UPAs</span>
-            </a>
-          </li>
-          <li role="presentation">
-            <a
-              href="#avaliacao"
-              aria-controls="avaliacao"
-              role="tab"
-              data-toggle="tab"
-            >
-              <span className="tabsNavigation">Avaliações UPAs</span>
-            </a>
-          </li>
-        </ul>
-        {/* </div> */}
-        <div className="tab-content">
-          <div role="tabpanel" className="tab-pane active" id="disponiveis">
-            <div className="conteiner">
-              <div className="area">
-                <img src={Bitmap} className="centro" alt="" />
+          </li> */}
+        <li role="presentation">
+          <a
+            href="#graficoatend"
+            aria-controls="graficoatend"
+            role="tab"
+            data-toggle="tab"
+          >
+            <span className="tabsNavigation">Gráfico Atendimentos</span>
+          </a>
+        </li>
+        <li role="presentation">
+          <a
+            href="#endereco"
+            aria-controls="endereco"
+            role="tab"
+            data-toggle="tab"
+          >
+            <span className="tabsNavigation">Endereços UPAs</span>
+          </a>
+        </li>
+        <li role="presentation">
+          <a
+            href="#avaliacao"
+            aria-controls="avaliacao"
+            role="tab"
+            data-toggle="tab"
+          >
+            <span className="tabsNavigation">Avaliações UPAs</span>
+          </a>
+        </li>
+      </ul>
+      {/* </div> */}
+      <div className="tab-content">
+        <div role="tabpanel" className="tab-pane active" id="disponiveis">
+          <div className="conteiner">
+            <div className="area">
 
-                <img src={Logo} className="logo" alt=""/>
+              <img src={Bitmap} className="centro" alt="" />
 
-                <ButtonMap  //btnCentro
-                  backgroundColorButton="rgb(204, 220, 226)"
-                  borderRadiusButton="5px"
-                  positionButton="absolute"
-                  heigthButton={27}
-                  widthButton={31}
-                  topButton={457}
-                  leftButton={1125}
-                  typeButton="submit"
-                  onClickButton={() => setShowModal(true)}
-                />
+              <img src={Logo} className="logo" alt="" />
 
-                <ButtonMap  //btnRocinha
-                  backgroundColorButton="rgb(204, 220, 226)"
-                  borderRadiusButton="5px"
-                  positionButton="absolute"
-                  heigthButton={27}
-                  widthButton={31}
-                  topButton={528}
-                  leftButton={1114}
-                  typeButton="submit"
-                  onClickButton={() => setShowModal(true)}
-                />
+              <ButtonMap  //btnCentro
+                backgroundColorButton="rgb(204, 220, 226)"
+                borderRadiusButton="5px"
+                positionButton="absolute"
+                heigthButton={27}
+                widthButton={31}
+                topButton={457}
+                leftButton={1125}
+                typeButton="submit"
+                onClickButton={() => setShowModal(true)}
+              />
+
+              <ButtonMap  //btnRocinha
+                backgroundColorButton="rgb(204, 220, 226)"
+                borderRadiusButton="5px"
+                positionButton="absolute"
+                heigthButton={27}
+                widthButton={31}
+                topButton={528}
+                leftButton={1114}
+                typeButton="submit"
+                onClickButton={() => setShowModal(true)}
+              />
+
+              <ButtonMap  //btnTijuca
+                backgroundColorButton="rgb(204, 220, 226)"
+                borderRadiusButton="5px"
+                positionButton="absolute"
+                heigthButton={27}
+                widthButton={31}
+                topButton={500}
+                leftButton={1062}
+                typeButton="submit"
+                onClickButton={() => setShowModal(true)}
+              />
+
+              <ButtonMap  //btnLins
+                backgroundColorButton="rgb(204, 220, 226)"
+                borderRadiusButton="5px"
+                positionButton="absolute"
+                heigthButton={27}
+                widthButton={31}
+                topButton={443}
+                leftButton={1032}
+                typeButton="submit"
+                onClickButton={() => setShowModal(true)}
+              />
+
+              <ButtonMap  //btnPenha
+                backgroundColorButton="rgb(204, 220, 226)"
+                borderRadiusButton="5px"
+                positionButton="absolute"
+                heigthButton={27}
+                widthButton={31}
+                topButton={377}
+                leftButton={1033}
+                typeButton="submit"
+                onClickButton={() => setShowModal(true)}
+              />
+
+              <ButtonMap  //btnMadureira
+                backgroundColorButton="rgb(204, 220, 226)"
+                borderRadiusButton="5px"
+                positionButton="absolute"
+                heigthButton={27}
+                widthButton={31}
+                topButton={387}
+                leftButton={968}
+                typeButton="submit"
+                onClickButton={() => setShowModal(true)}
+              />
+
+              <ButtonMap  //btnBangu
+                backgroundColorButton="rgb(204, 220, 226)"
+                borderRadiusButton="5px"
+                positionButton="absolute"
+                heigthButton={27}
+                widthButton={31}
+                topButton={422}
+                leftButton={866}
+                typeButton="submit"
+                onClickButton={() => setShowModal(true)}
+              />
+
+              <ButtonMap  //btnBarra
+                backgroundColorButton="rgb(204, 220, 226)"
+                borderRadiusButton="5px"
+                positionButton="absolute"
+                heigthButton={27}
+                widthButton={31}
+                topButton={522}
+                leftButton={899}
+                typeButton="submit"
+                onClickButton={() => setShowModal(true)}
+              />
+
+              <ButtonMap  //btnCampoGrande
+                backgroundColorButton="rgb(204, 220, 226)"
+                borderRadiusButton="5px"
+                positionButton="absolute"
+                heigthButton={27}
+                widthButton={31}
+                topButton={494}
+                leftButton={741}
+                typeButton="submit"
+                onClickButton={() => setShowModal(true)}
+              />
+
+              <ButtonMap  //btnSantaCruz
+                backgroundColorButton="rgb(204, 220, 226)"
+                borderRadiusButton="5px"
+                positionButton="absolute"
+                heigthButton={27}
+                widthButton={31}
+                topButton={467}
+                leftButton={611}
+                typeButton="submit"
+                onClickButton={() => setShowModal(true)}
+              />
+              <div className="avalie">
+                <div className="form-group ">
+                  <label className="col-md-1 control-label">Avalie:</label>
+                  <div className="col-md-3">
+                    <select className="form-control" id="cidades" data-placeholder="Selecione a cidade">
+                      <option value="0"></option>
+                      <option value="1">Centro</option>
+                      <option value="1">Rocinha</option>
+                      <option value="2">Lins</option>
+                      <option value="3">Penha</option>
+                      <option value="4">Tijuca</option>
+                      <option value="5">Madureira</option>
+                      <option value="6">Barra da Tijuca</option>
+                      <option value="7">Bangu</option>
+                      <option value="8">Campo Grande</option>
+                      <option value="9">Santa Cruz</option>
+                    </select>
+                  </div>
+                </div>
                 
-                <ButtonMap  //btnTijuca
-                  backgroundColorButton="rgb(204, 220, 226)"
-                  borderRadiusButton="5px"
-                  positionButton="absolute"
-                  heigthButton={27}
-                  widthButton={31}
-                  topButton={500}
-                  leftButton={1062}
-                  typeButton="submit"
-                  onClickButton={() => setShowModal(true)}
-                />
-
-                <ButtonMap  //btnLins
-                  backgroundColorButton="rgb(204, 220, 226)"
-                  borderRadiusButton="5px"
-                  positionButton="absolute"
-                  heigthButton={27}
-                  widthButton={31}
-                  topButton={443}
-                  leftButton={1032}
-                  typeButton="submit"
-                  onClickButton={() => setShowModal(true)}
-                />
+                <div>
+                  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"></link>
+                    <div className="estrelas">
+                      <input type="radio" id="cm_star-empty" name="fb" value="" checked />
+                      <label htmlFor="cm_star-1"><i className="fa"></i></label>
+                      <input type="radio" id="cm_star-1" name="fb" value="1" />
+                      <label htmlFor="cm_star-2"><i className="fa"></i></label>
+                      <input type="radio" id="cm_star-2" name="fb" value="2" />
+                      <label htmlFor="cm_star-3"><i className="fa"></i></label>
+                      <input type="radio" id="cm_star-3" name="fb" value="3" />
+                      <label htmlFor="cm_star-4"><i className="fa"></i></label>
+                      <input type="radio" id="cm_star-4" name="fb" value="4" />
+                      <label htmlFor="cm_star-5"><i className="fa"></i></label>
+                      <input type="radio" id="cm_star-5" name="fb" value="5" />
+                    </div>
                 
-                <ButtonMap  //btnPenha
-                  backgroundColorButton="rgb(204, 220, 226)"
-                  borderRadiusButton="5px"
-                  positionButton="absolute"
-                  heigthButton={27}
-                  widthButton={31}
-                  topButton={377}
-                  leftButton={1033}
-                  typeButton="submit"
-                  onClickButton={() => setShowModal(true)}
-                />
-                
-                <ButtonMap  //btnMadureira
-                  backgroundColorButton="rgb(204, 220, 226)"
-                  borderRadiusButton="5px"
-                  positionButton="absolute"
-                  heigthButton={27}
-                  widthButton={31}
-                  topButton={387}
-                  leftButton={968}
-                  typeButton="submit"
-                  onClickButton={() => setShowModal(true)}
-                />
+                </div>
+                </div>
              
-                <ButtonMap  //btnBangu
-                  backgroundColorButton="rgb(204, 220, 226)"
-                  borderRadiusButton="5px"
-                  positionButton="absolute"
-                  heigthButton={27}
-                  widthButton={31}
-                  topButton={422}
-                  leftButton={866}
-                  typeButton="submit"
-                  onClickButton={() => setShowModal(true)}
-                />
-
-                <ButtonMap  //btnBarra
-                  backgroundColorButton="rgb(204, 220, 226)"
-                  borderRadiusButton="5px"
-                  positionButton="absolute"
-                  heigthButton={27}
-                  widthButton={31}
-                  topButton={522}
-                  leftButton={899}
-                  typeButton="submit"
-                  onClickButton={() => setShowModal(true)}
-                />            
-               
-                <ButtonMap  //btnCampoGrande
-                  backgroundColorButton="rgb(204, 220, 226)"
-                  borderRadiusButton="5px"
-                  positionButton="absolute"
-                  heigthButton={27}
-                  widthButton={31}
-                  topButton={494}
-                  leftButton={741}
-                  typeButton="submit"
-                  onClickButton={() => setShowModal(true)}
-                /> 
-
-                <ButtonMap  //btnSantaCruz
-                  backgroundColorButton="rgb(204, 220, 226)"
-                  borderRadiusButton="5px"
-                  positionButton="absolute"
-                  heigthButton={27}
-                  widthButton={31}
-                  topButton={467}
-                  leftButton={611}
-                  typeButton="submit"
-                  onClickButton={() => setShowModal(true)}
-                /> 
               </div>
 
 
@@ -232,46 +310,19 @@ const Home: React.FC = () => {
                   </span>
                 </h4>
               </div>
-              <DistrictUpa
-                DistrictTitle = "Centro"
-                NumberOfAttendance = {1800}
-              />
-              <DistrictUpa
-                DistrictTitle = "Rocinha"
-                NumberOfAttendance = {1150}
-              />
-              <DistrictUpa
-                DistrictTitle = "Lins"
-                NumberOfAttendance = {950}
-              />
-              <DistrictUpa
-                DistrictTitle = "Penha"
-                NumberOfAttendance = {1100}
-              />
-              <DistrictUpa
-                DistrictTitle = "Tijuca"
-                NumberOfAttendance = {1100}
-              />
-              <DistrictUpa
-                DistrictTitle = "Madureira"
-                NumberOfAttendance = {1300}
-              />
-              <DistrictUpa
-                DistrictTitle = "Barra da Tijuca"
-                NumberOfAttendance = {800}
-              />
-              <DistrictUpa
-                DistrictTitle = "Bangu"
-                NumberOfAttendance = {1500}
-              />
-              <DistrictUpa
-                DistrictTitle = "Campo Grande"
-                NumberOfAttendance = {1400}
-              />
-              <DistrictUpa
-                DistrictTitle = "Santa Cruz"
-                NumberOfAttendance = {1600}
-              />
+
+              {districtUpas.map((upa, index) => {
+                return (
+                  <DistrictUpa
+                    key={upa.title.toString()}
+                    DistrictTitle={upa.title}
+                    NumberOfAttendance={upa.atendimentos}
+                  />
+                )
+              })}
+
+
+
             </div>
           </div>
         </div>
@@ -294,10 +345,10 @@ const Home: React.FC = () => {
         </div>
 
 
-{/*Adicionar informações de cada upa.
+        {/*Adicionar informações de cada upa.
 //adicionar um array que vai renderizar essas informações
 */}
-{/* O componente foi criado */}
+        {/* O componente foi criado */}
 
         <div role="tabpanel" className="tab-pane" id="endereco">
           <div className="itemEnd">
